23fa9a2c6b3d2a00776e4223d78c8526faef506d
Zearain <martinba88@gmail.com>
2023-12-01 20:49:33 +0000

    Attempting to kill the database

---
+++ src/WebAPI/Controllers/AoC23DbContextController.cs
@@ -0,0 +1,40 @@
+﻿// <copyright file="AoC23DbContextController.cs" company="Zearain">
+// Copyright (c) Zearain. All rights reserved.
+// </copyright>
+
+using Microsoft.AspNetCore.Mvc;
+
+using Zearain.AoC23.Infrastructure.Persistence;
+
+namespace Zearain.AoC23.WebAPI;
+
+/// <summary>
+/// Represents a controller for the AoC23DbContext.
+/// </summary>
+[Route("api/[controller]")]
+[ApiController]
+public class AoC23DbContextController : ControllerBase
+{
+    private readonly AoC23DbContext context;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="AoC23DbContextController"/> class.
+    /// </summary>
+    /// <param name="context">The context to use.</param>
+    public AoC23DbContextController(AoC23DbContext context)
+    {
+        this.context = context;
+    }
+
+    /// <summary>
+    /// Gets the AoC23DbContext.
+    /// </summary>
+    /// <returns>A <see cref="Task{TResult}"/> representing the result of the asynchronous operation.</returns>
+    [HttpGet("reset")]
+    public async Task<IActionResult> Reset()
+    {
+        await this.context.Database.EnsureDeletedAsync();
+        await this.context.Database.EnsureCreatedAsync();
+        return this.Ok();
+    }
+}
