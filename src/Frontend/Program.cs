a95d916dbb79218c1905a267c5f4fc1cb5430106
Martin Bakke <martinba88@gmail.com>
2023-12-01 17:02:57 +0000

    Started on frontend

--- src/Frontend/Program.cs
+++ src/Frontend/Program.cs
@@ -4,6 +4,9 @@
 
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+using MudBlazor.Services;
+
 using Zearain.AoC23.Frontend;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -11,5 +14,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
